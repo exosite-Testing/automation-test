0802ccac02b6ca54ba758bbacaa40d7189ebf638
Edward Wang <edwardwang@exosite.com>
2016-09-12 09:38:00 +0800

    add error endpoint

--- sample_api.lua
+++ sample_api.lua
@@ -1,2 +1,9 @@
 --#ENDPOINT GET /default
 response.message = "I'm default endpoint"
+--#ENDPOINT GET /cause/error
+local data = {
+  ["request_id"] = "wsRid",
+  ["server_ip"] = "wsSip",
+  message = "wsMsg"
+}
+return Websocket.send(data)
