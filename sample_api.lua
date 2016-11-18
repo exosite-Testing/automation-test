3987f4fbbdaaa9287dbdcaf27f16803e38530def
Edward Wang <edwardwang@exosite.com>
2016-11-18 10:46:10 +0800

    Revert "Add init endpoint (Issue: DQA-743)"
    This reverts commit af198f71219e9a43280554ca9c745f6200fc9f85.

--- sample_api.lua
+++ sample_api.lua
@@ -7,16 +7,3 @@ local data = {
   message = "wsMsg"
 }
 return Websocket.send(data)
---#ENDPOINT GET /_init
-local ret1 = User.createRole({role_id = "engineer", parameter = {{name = "sn"}}})
-local ret2 = User.createRole({role_id = "normal", parameter = {{name = "sn"}}})
-local ret = ret1.status_code ~= nil and ret1 or nil
-if ret == nil then
-  ret = ret2.status_code ~= nil and ret2 or nil
-end
-if ret ~= nil then
-  response.code = ret.status_code
-  response.message = ret.message
-else
-  response.code = 200
-end
