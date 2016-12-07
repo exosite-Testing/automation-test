7c7c819655ab6f8c193e0a1c8b031f0e73ad7510
Edward Wang <edwardwang@exosite.com>
2016-12-07 10:28:43 +0800

    Add endpoint of four content type (Issue: DQA-745)

--- sample_api.lua
+++ sample_api.lua
@@ -7,3 +7,35 @@ local data = {
   message = "wsMsg"
 }
 return Websocket.send(data)
+--#ENDPOINT POST /text/plain text/plain
+return request.body
+--#ENDPOINT POST /application/xwwwFormUrlencoded application/x-www-form-urlencoded
+return request.body
+--#ENDPOINT POST /application/octetStream application/octet-stream
+return request.body
+--#ENDPOINT POST /application/json application/json
+return request.body
+--#ENDPOINT PUT /text/plain text/plain
+return request.body
+--#ENDPOINT PUT /application/xwwwFormUrlencoded application/x-www-form-urlencoded
+return request.body
+--#ENDPOINT PUT /application/octetStream application/octet-stream
+return request.body
+--#ENDPOINT PUT /application/json application/json
+return request.body
+--#ENDPOINT PATCh /text/plain text/plain
+return request.body
+--#ENDPOINT PATCh /application/xwwwFormUrlencoded application/x-www-form-urlencoded
+return request.body
+--#ENDPOINT PATCh /application/octetStream application/octet-stream
+return request.body
+--#ENDPOINT PATCh /application/json application/json
+return request.body
+--#ENDPOINT DELETE /text/plain text/plain
+return request.body
+--#ENDPOINT DELETE /application/xwwwFormUrlencoded application/x-www-form-urlencoded
+return request.body
+--#ENDPOINT DELETE /application/octetStream application/octet-stream
+return request.body
+--#ENDPOINT DELETE /application/json application/json
+return request.body
